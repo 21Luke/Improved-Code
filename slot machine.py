2323b6c5ad516b513ca37edfdb394bbc77f4fce5
21Luke <lukeskar2002@gmail.com>
2022-12-09 22:09:39 -0600

    first file

---
+++ slot machine.py
@@ -0,0 +1,18 @@
+from slot_machine_gui import *
+
+def main()-> None:
+    """
+    function to initialize the program
+
+    """
+
+    window = Tk()
+    window.title('Slot Machine')
+    window.geometry('400x300')
+    window.resizable(False, False)
+    GUI(window)
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
